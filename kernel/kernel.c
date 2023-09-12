88d66608428fe2bd51ea32c7bcff31d771d84424
An Do <s3878698@rmit.edu.vn>
2023-09-12 14:41:14 +0700

    done with team info

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -108,25 +108,27 @@ void displayTeamInfo() {
 
     freeMemory(groupEighteen);
 
-    // Le Pham Ngoc Hieu
-    unsigned long *studentOneName[18]  = {epd_bitmap_l, epd_bitmap_e, epd_bitmap_space , epd_bitmap_p, epd_bitmap_h, epd_bitmap_a, epd_bitmap_m, epd_bitmap_space , epd_bitmap_n, epd_bitmap_g, epd_bitmap_o, epd_bitmap_c, epd_bitmap_space , epd_bitmap_h, epd_bitmap_i, epd_bitmap_e, epd_bitmap_u};
-    drawStrScaledDown(10, 100, studentOneName, 100, 100, 0xFFFF0000, 0xFF0000FF, 4);
+    // Le Pham Ngoc Hieu : S3877375
+    unsigned long *studentOneName[30]  = {epd_bitmap_l, epd_bitmap_e, epd_bitmap_space , epd_bitmap_p, epd_bitmap_h, epd_bitmap_a, epd_bitmap_m, epd_bitmap_space , epd_bitmap_n, epd_bitmap_g, epd_bitmap_o, epd_bitmap_c, epd_bitmap_space , epd_bitmap_h, epd_bitmap_i, epd_bitmap_e, epd_bitmap_u, epd_bitmap_colon, epd_bitmap_s, epd_bitmap_num3, epd_bitmap_num8, epd_bitmap_num7, epd_bitmap_num7, epd_bitmap_num3, epd_bitmap_num7, epd_bitmap_num5};
+    drawStrScaledDown(10, 100, studentOneName, 100, 100, COLOR_ORANGE, COLOR_WHITE, 4);
 
     freeMemory(studentOneName);
 
-    unsigned long *studentTwoName[14]  = {epd_bitmap_m, epd_bitmap_a, epd_bitmap_i, epd_bitmap_space , epd_bitmap_c, epd_bitmap_h, epd_bitmap_i, epd_bitmap_e, epd_bitmap_u, epd_bitmap_space , epd_bitmap_t, epd_bitmap_h, epd_bitmap_u, epd_bitmap_y};
-    drawStrScaledDown(10, 150, studentTwoName, 100, 100, 0xFFFF0000, 0xFF0000FF, 4);
+    // Mai Chieu Thuy : s3877746
+    unsigned long *studentTwoName[30]  = {epd_bitmap_m, epd_bitmap_a, epd_bitmap_i, epd_bitmap_space , epd_bitmap_c, epd_bitmap_h, epd_bitmap_i, epd_bitmap_e, epd_bitmap_u, epd_bitmap_space , epd_bitmap_t, epd_bitmap_h, epd_bitmap_u, epd_bitmap_y, epd_bitmap_colon, epd_bitmap_s, epd_bitmap_num3, epd_bitmap_num8, epd_bitmap_num7, epd_bitmap_num7, epd_bitmap_num4, epd_bitmap_num6};
+    drawStrScaledDown(10, 150, studentTwoName, 100, 100, COLOR_BLUE, COLOR_WHITE, 4);
     
     freeMemory(studentTwoName);
 
-    unsigned long *studentThreeName[13]  = {epd_bitmap_p, epd_bitmap_a, epd_bitmap_r, epd_bitmap_k, epd_bitmap_space , epd_bitmap_a, epd_bitmap_n, epd_bitmap_h, epd_bitmap_space , epd_bitmap_k, epd_bitmap_i, epd_bitmap_e, epd_bitmap_t};
-    drawStrScaledDown(10, 200, studentThreeName, 100, 100, 0xFFFF0000, 0xFF0000FF, 4);
+    // Park Anh Kiet : s3681475
+    unsigned long *studentThreeName[25]  = {epd_bitmap_p, epd_bitmap_a, epd_bitmap_r, epd_bitmap_k, epd_bitmap_space , epd_bitmap_a, epd_bitmap_n, epd_bitmap_h, epd_bitmap_space , epd_bitmap_k, epd_bitmap_i, epd_bitmap_e, epd_bitmap_t, epd_bitmap_colon, epd_bitmap_s, epd_bitmap_num3, epd_bitmap_num6, epd_bitmap_num8, epd_bitmap_num1, epd_bitmap_num4, epd_bitmap_num7, epd_bitmap_num5};
+    drawStrScaledDown(10, 200, studentThreeName, 100, 100, COLOR_RED, COLOR_WHITE, 4);
 
     freeMemory(studentThreeName);
 
     // DO TRUONG AN : s3878698
     unsigned long *studentFourName[25]  = {epd_bitmap_d, epd_bitmap_o, epd_bitmap_space , epd_bitmap_t, epd_bitmap_r, epd_bitmap_u, epd_bitmap_o, epd_bitmap_n, epd_bitmap_g, epd_bitmap_space ,epd_bitmap_a, epd_bitmap_n, epd_bitmap_colon, epd_bitmap_s, epd_bitmap_num3, epd_bitmap_num8, epd_bitmap_num7, epd_bitmap_num8, epd_bitmap_num6, epd_bitmap_num9, epd_bitmap_num8};
-    drawStrScaledDown(10, 250, studentFourName, 100, 100, 0xFFFF0000, 0xFF0000FF, 4);
+    drawStrScaledDown(10, 250, studentFourName, 100, 100, COLOR_BLACK, COLOR_WHITE, 4);
 
     freeMemory(studentFourName);
 
